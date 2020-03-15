d0a18158c73d6f438bdfd88b580fbd881120eb60
katto <h_ju@qq.com>
2020-03-15 15:24:58 +0800

    index HTML和bootstrap添加

--- src/main/java/com/example/demo4/Demo4Application.java
+++ src/main/java/com/example/demo4/Demo4Application.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Demo4Application {
 
+	public static final String currentVersion = "V1";
+
 	public static void main(String[] args) {
 		SpringApplication.run(Demo4Application.class, args);
 	}
---
+++ src/main/java/com/example/demo4/controller/indexController.java
@@ -0,0 +1,20 @@
+package com.example.demo4.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * @ClassName indexController
+ * @Description TODO
+ * @Author Administrator
+ * @Date 2020/3/15 14:11
+ */
+
+@Controller
+public class indexController {
+
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+}
